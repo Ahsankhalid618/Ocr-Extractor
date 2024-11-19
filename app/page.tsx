4af89f0aa3f5501a350d8e444ba876ef8427f692
Ahsan khalid <ahsankhalid816@gmail.com>
2024-11-20 04:23:15 +0500

    Advancee UI updated.

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,20 @@
+"use client";
+
 import { ImageUploader } from "@/components/image-uploader";
 import { ModeToggle } from "@/components/mode-toggle";
-import { ScanText, ChevronRight } from "lucide-react";
+import { ScanText } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { RainbowButton } from "@/components/ui/rainbow-button";
 import SparklesText from "@/components/ui/sparkles-text";
 import Image from "next/image";
-import AvatarImg from "../public/avatar.jpg"
+import AvatarImg from "../public/avatar.jpg";
 import Link from "next/link";
+import { motion } from "framer-motion";
+import { HeroHighlight, Highlight } from "@/components/ui/hero-highlight";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-background">
+    <>
       <nav className="border-b">
         <div className="container flex h-16 items-center px-4">
           <div className="flex items-center space-x-2">
@@ -19,9 +23,13 @@ export default function Home() {
           </div>
 
           <div className="flex-1 flex justify-center ">
-            <Link href="https://linktr.ee/Ahsan_Khalid" target="_blank" rel="noopener noreferrer">
-            <RainbowButton className="flex justify-around h-[2.2rem] dark:bg-slate-800">
-            <Image
+            <Link
+              href="https://linktr.ee/Ahsan_Khalid"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <RainbowButton className="flex justify-around h-[2.2rem] dark:bg-slate-800">
+                <Image
                   className="h-auto rounded-full"
                   src={AvatarImg}
                   alt="Profile image"
@@ -29,19 +37,16 @@ export default function Home() {
                   height={28}
                   aria-hidden="true"
                 />
-            <hr className="mx-2 h-4 w-px shrink-0 bg-gray-300" />{" "}
-              <span
-                className={cn(
-                  `inline animate-gradient bg-gradient-to-r from-[#ffaa40] via-[#9c40ff] to-[#ffaa40] bg-[length:var(--bg-size)_100%] bg-clip-text text-transparent`
-                )}
-              >
-                @Ahsan_Khalid
-                
-              </span>
-              
+                <hr className="mx-2 h-4 w-px shrink-0 bg-gray-300" />{" "}
+                <span
+                  className={cn(
+                    `inline animate-gradient bg-gradient-to-r from-[#ffaa40] via-[#9c40ff] to-[#ffaa40] bg-[length:var(--bg-size)_100%] bg-clip-text text-transparent`
+                  )}
+                >
+                  @Ahsan_Khalid
+                </span>
               </RainbowButton>
             </Link>
-          
           </div>
           <div className="ml-auto">
             <ModeToggle />
@@ -49,23 +54,43 @@ export default function Home() {
         </div>
       </nav>
 
-      <div className="container px-4 py-8">
-        <div className="mx-auto max-w-3xl">
-          <div className="mb-8 text-center">
-            {/* <h1 className="text-4xl font-bold tracking-tight">
-              Extract Text from Images
-            </h1> */}
-            <SparklesText className="text-4xl" text="Extract Text from Images" />
-            <p className="mt-4 text-muted-foreground">
-              Upload an image and we&apos;ll extract the text for you. You can
-              then convert it to speech, download as PDF, or export to Google
-              Docs.
-            </p>
-          </div>
+      <HeroHighlight>
+        <div className="container px-4 py-8">
+          <div className="mx-auto max-w-3xl">
+            <div className="mb-8 text-center">
+              <SparklesText
+                className="text-4xl"
+                text="Extract Text from Images"
+              />
+              <motion.p
+                initial={{
+                  opacity: 0,
+                  y: 20,
+                }}
+                animate={{
+                  opacity: 1,
+                  y: [20, -5, 0],
+                }}
+                transition={{
+                  duration: 0.5,
+                  ease: [0.4, 0.0, 0.2, 1],
+                }}
+                className="mt-4 text-muted-foreground"
+              >
+                Upload an image and we&apos;ll extract the text for you. You can
+                then convert it to 
+                <br/>
+                <Highlight className="text-black dark:text-white">
+                speech, download as PDF, or export to Google Docs
+                </Highlight>
+                .
+              </motion.p>
+            </div>
 
-          <ImageUploader />
+            <ImageUploader />
+          </div>
         </div>
-      </div>
-    </main>
+      </HeroHighlight>
+    </>
   );
 }
